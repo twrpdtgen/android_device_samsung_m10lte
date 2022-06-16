8b4ac2400feae352af391c62a26bb9f425c68386
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-16 22:43:50 +0200

    m10lte: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-M105G Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m10lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m10lte-user \
+    omni_m10lte-userdebug \
+    omni_m10lte-eng
